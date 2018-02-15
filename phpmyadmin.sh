f031d78f023a71278f65eac008b8719fba8683b0
Bretfelean Sorin Cristian <36470944+dev-nowornet@users.noreply.github.com>
2018-02-15 20:17:51 +0100

    Delete phpmyadmin.sh

--- phpmyadmin.sh
+++
@@ -1,7 +0,0 @@
-
-cd /usr/share/doc/phpmyadmin/examples
-sudo gunzip create_tables.sql.gz
-mysql -u root -p < create_tables.sql
-mysql -u root -p -e 'GRANT SELECT, INSERT, DELETE, UPDATE ON phpmyadmin.* TO 'pma'@'localhost' IDENTIFIED BY "pmapass"'
-sudo sed -i 's/\/\/\ \$cfg\[\'Servers\'\]\[\$i\]\[\'controluser\'\]/\$cfg\[\'Servers\'\]\[\$i\]\[\'controluser\'\]/g' /etc/phpmyadmin/config.inc.php
-sudo sed -i 's/\/\/\ \$cfg\[\'Servers\'\]\[\$i\]\[\'controlpass\'\]/\$cfg\[\'Servers\'\]\[\$i\]\[\'controlpass\'\]/g' /etc/phpmyadmin/config.inc.php
